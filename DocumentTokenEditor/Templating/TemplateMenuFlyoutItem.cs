3f1a0d94b4822a12902deff692485d03b6f739b2
Michael MiDri <riddlemd@gmail.com>
2023-11-17 11:15:12 -0600

    Code Cleanup

--- DocumentTokenEditor/Templating/TemplateMenuFlyoutItem.cs
+++ DocumentTokenEditor/Templating/TemplateMenuFlyoutItem.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DocumentTokenEditor.Templating
+﻿namespace DocumentTokenEditor.Templating
 {
     public class TemplateMenuFlyoutItem : MenuFlyoutItem
     {
